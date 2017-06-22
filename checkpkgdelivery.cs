46dda0ec95df77b3c8010a7fd9500d3827a25cf0
somewhatsomewhere <somewhatsomewhere@users.noreply.github.com>
2017-06-22 11:08:17 +0200

    Add build.bat into cs file for ease of use

--- checkpkgdelivery.cs
+++ checkpkgdelivery.cs
@@ -1,3 +1,14 @@
+/*
+: Build.bat to compile the tool in a given folder
+@echo off
+
+set csc=@c:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe
+
+cmd /c %csc% /out:checkpkgdelivery.exe checkpkgdelivery.cs
+
+*/
+
+
 using System;
 using System.IO;
 
